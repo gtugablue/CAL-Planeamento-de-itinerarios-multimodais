ef04fc208465f577958955ff51525d9fe27ec6cd
Ricardo <rcerqueira94@gmail.com>
2015-04-24 22:46:02 +0100

    brute force2

---
+++ Planeamento de itinerarios multimodais/source/BruteForce.cpp
@@ -0,0 +1,36 @@
+#include "BruteForce.h"
+
+using namespace std;
+
+bool brute_force_aux(Graph* g, Vertex* ini, Vertex* f, Path* current, Path* min){
+	if(current->getCost() > min->getCost())
+		return false;
+	if(ini == f){
+		*min = *current;
+		return true;
+	}
+	for(int i = 0; i < ini->getAdj().size(); i++){
+		if(!ini->getAdj()[i]->getDst()->getProcessed()){
+			ini->getAdj()[i]->getDst()->incProcessed();
+			current->addEdgeEnd(ini->getAdj()[i]);
+			double savedCost = current->getCost();
+			current->setCost(savedCost + ini->getAdj()[i]->getWeight());
+			brute_force_aux(g, ini->getAdj()[i]->getDst(),  f, current, min);
+			current->setCost(savedCost);
+			current->removeEdgeEnd();
+			ini->getAdj()[i]->getDst()->resetProcessed();
+		}
+	}
+	return min->getCost() < DBL_MAX;
+}
+Path* brute_force(Graph* g, Vertex* ini, Vertex* f){
+	for(int i = 0; i < g->getVertexSet().size(); i++){
+		g->getVertexSet()[i]->resetProcessed();
+	}
+	Path* min = new Path(DBL_MAX);
+	Path* current = new Path(0);
+	brute_force_aux(g, ini, f, current, min);
+	return min;
+}
+
+
---
+++ Planeamento de itinerarios multimodais/source/BruteForce.h
@@ -0,0 +1,10 @@
+#ifndef BRUTE_FORCE_H
+#define BRUTE_FORCE_H
+
+#include "Graph.h"
+#include "Path.h"
+#include <vector>
+
+Path* brute_force(Graph* g, Vertex* ini, Vertex* f);
+
+#endif
