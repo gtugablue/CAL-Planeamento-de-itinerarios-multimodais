cdd5e904a281c0e4955f8674570c6bf74a8252eb
Ricardo <rcerqueira94@gmail.com>
2015-04-24 17:24:14 +0100

    bugs

--- Planeamento de itinerarios multimodais/source/main.cpp
+++ Planeamento de itinerarios multimodais/source/main.cpp
@@ -265,8 +265,8 @@ int main(int argc, char* argv[]) {
 	}
 
 	Graph* g1 = GraphGen::randGraph(10,17,50, 750, 50, 550);
-	Path* p = dijsktra(g1, g1->getVertexSet()[0], g1->getVertexSet()[1]);
-	Path* p2 = astar(g1, g1->getVertexSet()[0], g1->getVertexSet()[1]);
+	Path* p = dijsktra_fib(g1, g1->getVertexSet()[0], g1->getVertexSet()[1]);
+	Path* p2 = astar_fib(g1, g1->getVertexSet()[0], g1->getVertexSet()[1]);
 	cerr << "yay1\n";
 	SDL_Event e;
 	bool moving = false;
@@ -301,8 +301,8 @@ int main(int argc, char* argv[]) {
 			delete p;
 			delete p2;
 			g1 = GraphGen::randGraph(10,17,50, 750, 50, 550);
-			p = dijsktra(g1, g1->getVertexSet()[0], g1->getVertexSet()[1]);
-			p2 = astar(g1, g1->getVertexSet()[0], g1->getVertexSet()[1]);
+			p = dijsktra_fib(g1, g1->getVertexSet()[0], g1->getVertexSet()[1]);
+			p2 = astar_fib(g1, g1->getVertexSet()[0], g1->getVertexSet()[1]);
 
 		}
 		else if(e.type  ==  SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_RIGHT){
