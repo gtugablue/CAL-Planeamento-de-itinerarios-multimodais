4362a344c855777b826289c6e056c6bb89059a97
aslmbc13 <asl_mbc_13@hotmail.com>
2015-04-24 17:02:55 +0100

    Added check for different paths in both algorithms

--- Planeamento de itinerarios multimodais/source/Path.h
+++ Planeamento de itinerarios multimodais/source/Path.h
@@ -26,6 +26,19 @@ public:
 	void addEdgeBeginning(Edge* e){
 		edges.insert(edges.begin(), e);
 	}
+
+	bool operator==(const Path p2) const{
+		if(edges.size() != p2.edges.size())
+			return false;
+
+		for(int i = 0; i < edges.size(); i++)
+		{
+			if(edges[i] != p2.edges[i])
+				return false;
+		}
+
+		return true;
+	}
 };
 
 
